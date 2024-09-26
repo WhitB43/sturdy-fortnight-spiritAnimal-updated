79c1e68dfdee8b9d152e584cce21500a33160931
WhitB43 <fwrdaerial@gmail.com>
2024-09-26 13:54:19 -0500

    Add files via upload
    Updated version of the spirit animal repository

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("in console...")
+
+$('#btnToggle').on('click', function() {
+  console.log("event?: ", "toggle clicked");
+  
+});
+
+// $('body').css("background-color", "yellow");
+
+// when button is clicked
+//IIFE = immediately invoked f/n expression 
+$("#btnGetAnimal").on("click", function(){
+  //get the user name
+  let bakerName = $("#bakerName").val(); 
+  //var variableName =  ... let (const) 
+  
+  $("#greetingArea").text("Hi there, " + bakerName + "!");  
+  //give them a personalized greeting
+  
+  let randomAnimal = spiritAnimals[(Math.floor(Math.random() * spiritAnimals.length))];
+  
+  $("#animalArea").text("Your spirit animal is: " + randomAnimal)
+  //next, also tell spirit animal 
+} )
+
+let spiritAnimals = ["🦅","🌵","🐟","🐛","👾","🦆","file not found"]
